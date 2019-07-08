a7bab3c7e672a8e58160fb57d7fcb3ac49b94cc6
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-08 12:15:39 +0900

    2.1 2D Context

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 let painting = false;
+function stopPainting() {
+  painting = false;
+}
+function startPainting() {
+  painting = true;
+}
 function onMouseMove(event) {
   // console.log(event)
   // offset X, Y: canvas 내에서의 x, y좌표
   // client X, Y: 전체 내에서의 x, y 좌표
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 function onMouseDown(event) {
   painting = true;
 }
-function onMouseUp(event) {
-  painting = false;
-}
-function onMouseLeave(event) {
-  painting = false;
-}
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown); // 클릭했을 때 발생하는 이벤트
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", onMouseLeave);
+  canvas.addEventListener("mousedown", startPainting); // 클릭했을 때 발생하는 이벤트
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
